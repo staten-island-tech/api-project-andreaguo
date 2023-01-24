00872cb14870c3c4850e731de91d8795d5df46ae
andreaguo <113132239+andreaguo@users.noreply.github.com>
2023-01-24 15:31:22 -0500

    asdfjkl

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -24,8 +24,16 @@ import '../styles/style.css';
 //     suzie.then((result)=>{   // shows the result/product
 //         console.log(result);       
 //     });
+const DOMSelectors={
+    home: document.querySelector(".home"),
+    male: document.querySelector(".male"),
+    female: document.querySelector(".female"),
+    human: document.querySelector(".human"),
+    alien: document.querySelector(".alien"),
 
-const URL = "https://rickandmortyapi.com/api/character/";
+};
+
+const URL = "https://rickandmortyapi.com/api/character/"
 
 async function getData(URL){
     try {
@@ -35,17 +43,20 @@ async function getData(URL){
             throw error(response);
         } else {
             const data = await response.json();
-            cards(data);
+            const array =  data.results;
+            const malefilter = filterGender(array, "male");
+            const array1 = Array.from(malefilter);
+            cards(array);
+            homeBTN(array);
+            maleBTN(array1);
         }    
     } catch (error) {
         console.log(error);
     }
 }
 
-function cards(data){
-    const array =  data.results;
+function cards(array){
         console.log(array);
-        console.log(data);
         array.forEach((el)=> document.getElementById("cold").insertAdjacentHTML(
             "afterbegin",
             `<div class="flip-card">
@@ -65,5 +76,28 @@ function cards(data){
         )) 
 }
 
+function homeBTN(array){
+    DOMSelectors.home.addEventListener("click", function(){
+    document.querySelectorAll(".flip-card").forEach((e)=> e.remove());
+    cards(array);         
+})
+}
+//you only need one of the functions that allows any filter/button
+//that you want to press to filter through that when you pass that
+//specific property in through the paraeter
+// ie:
+// function button(home, array1)
+//const array1 = Array.from() <-- this will map a new array specific to each filter
+
+function maleBTN(array1){
+    DOMSelectors.male.addEventListener("click", function(){
+    document.querySelectorAll(".flip-card").forEach((e)=> e.remove()); 
+    cards(array1);
+    })
+}
+
+function filterGender(array, type){
+    return array.filter((el)=> el.gender === `${type}`)
+}
 
 getData(URL);
