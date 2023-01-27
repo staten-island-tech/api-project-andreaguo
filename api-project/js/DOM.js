f7c7717056b93019771ef94a1371eff153c7f01e
andreaguo <113132239+andreaguo@users.noreply.github.com>
2023-01-26 20:06:44 -0500

    asdfghjk

---
+++ api-project/js/DOM.js
@@ -0,0 +1,10 @@
+const DOMSelectors={
+    home: document.querySelector(".home"),
+    male: document.querySelector(".male"),
+    female: document.querySelector(".female"),
+    human: document.querySelector(".human"),
+    alien: document.querySelector(".alien"),
+
+};
+
+export { DOMSelectors };
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -25,16 +25,8 @@ import '../styles/style.css';
 //         console.log(result);       
 //     });
 
-import { cardFunctions, buttonFunctions } from "./functions.js";
+import { DOMSelectors } from "./DOM.js";
 
-const DOMSelectors={
-    home: document.querySelector(".home"),
-    male: document.querySelector(".male"),
-    female: document.querySelector(".female"),
-    human: document.querySelector(".human"),
-    alien: document.querySelector(".alien"),
-
-};
 
 const URL = "https://rickandmortyapi.com/api/character/"
 
@@ -61,7 +53,66 @@ async function getData(URL){
 
 getData(URL);
 
-document.querySelector(".color").addEventListener("click", function () {
+
+function cards(array){
+    console.log(array);
+    array.forEach((el)=> document.getElementById("cold").insertAdjacentHTML(
+        "afterbegin",
+        `<div class="flip-card">
+        <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <h3>${el.name}</h3>
+            <img src=${el.image} alt="character image">
+        </div>
+        <div class="flip-card-back">
+            <h3>${el.name}</h3>
+            <h3>${el.gender}</h3> 
+            <h3>${el.origin.name}</h3>
+        </div>
+        </div>
+        </div>
+    </div>`
+    )) 
+}
+
+function homeBTN(array){
+    DOMSelectors.home.addEventListener("click", function(){
+    remove();
+    cards(array);         
+})
+}
+
+function buttonG(DOM, array, type){
+    DOM.addEventListener("click", function(){
+    remove();
+    const name = filterGender(array, `${type}`)
+    const newArr = Array.from(name)
+    cards(newArr);
+    })
+}
+
+function buttonS(DOM, array, type){
+    DOM.addEventListener("click", function(){
+    remove();
+    const name = filterSpecies(array, `${type}`)
+    const newArr = Array.from(name)
+    cards(newArr);
+    })
+}
+
+function filterGender(array, type){
+    return array.filter((el)=> el.gender === `${type}`)
+}
+
+function filterSpecies(array, type){
+    return array.filter((el)=> el.species === `${type}`)
+}
+
+function remove(){
+    document.querySelectorAll(".flip-card").forEach((e)=> e.remove()); 
+}
+
+document.getElementById("picture").addEventListener("click", function () {
     if (document.body.classList.contains("cool")) {
       document.body.classList.add("warm");
       document.body.classList.remove("cool");
