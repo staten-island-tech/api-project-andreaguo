328ec278c1a6252b7f8dad60b2133f6430f87500
andreaguo <andrea.guo2007@gmail.com>
2023-04-19 14:04:22 -0400

    asdf

---
+++ file/index.js
@@ -0,0 +1,14 @@
+let totalCost = 4000
+let students = [0.5, 0.2, 0.1, 0.2]
+let totalStudents = 400
+let product = []
+let excess = []
+
+students.forEach((el)=>{
+    let value = el * totalStudents
+    if (Number.isInteger(value)) {
+        student.findIndex()
+      }
+      else{
+
+      }
